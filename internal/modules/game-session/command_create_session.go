a9b043525552d8e1bd8a854e9e3f9dc253d96ce4
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2023-01-26 20:38:41 +0100

    Replace product with game-session

---
+++ internal/modules/game-session/command_create_session.go
@@ -0,0 +1,56 @@
+package gamesession
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+)
+
+type CreateSessionCommand struct {
+	OwnerID uuid.UUID
+	Name    string
+}
+
+func (c CreateSessionCommand) Validate() error {
+	if c.OwnerID == uuid.Nil {
+		return fmt.Errorf("invalid OwnerID - %s", c.OwnerID.String())
+	}
+
+	return nil
+}
+
+type CreateSessionResponse struct {
+	SessionID string
+}
+
+type CreateSessionCommandHandler struct {
+	db *sqlx.DB
+}
+
+func NewCreateSessionCommandHandler(db *sqlx.DB) *CreateSessionCommandHandler {
+	return &CreateSessionCommandHandler{db}
+}
+
+func (h *CreateSessionCommandHandler) Handle(
+	ctx context.Context,
+	request CreateSessionCommand,
+) (CreateSessionResponse, error) {
+	session := Session{
+		ID:      uuid.New().String(),
+		OwnerID: request.OwnerID,
+	}
+
+	const stmt = `
+		INSERT INTO
+			game_session(id, owner_id, name)
+		VALUES
+			(:id, :owner_id, :name);`
+
+	if _, err := h.db.NamedExecContext(ctx, stmt, session); err != nil {
+		return CreateSessionResponse{}, err
+	}
+
+	return CreateSessionResponse{SessionID: session.ID}, nil
+}
